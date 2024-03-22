be71236cf3cc45f95c25f253b3c19a3811eaffa9
Dell <Dell@DESKTOP-2L70K5E>
2024-03-22 17:18:39 +0530

    First Commit

---
+++ src/main/java/Guna/Automation/Sort.java
@@ -0,0 +1,37 @@
+package Guna.Automation;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Desktop\\GG\\chromedriver-win64\\chromedriver.exe");
+		  ChromeDriver driver = new ChromeDriver();
+		  
+		  driver.get("https://www.saucedemo.com/v1/");
+		  driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		  driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		  driver.findElement(By.id("login-button")).click();
+		 driver.findElement(By.className("bm-burger-button")).click();
+		  
+	  List <WebElement> menu = driver.findElements(By.className("product_sort_container"));
+		  
+		  Iterator<WebElement> i = menu.iterator();	  
+		while(i.hasNext())
+		{
+			
+			String s = i.next().getText();
+			System.out.println(s);
+		}
+
+		driver.close();
+		}
+
+	}
+
